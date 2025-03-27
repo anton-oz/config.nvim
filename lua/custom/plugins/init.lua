51626a62946d5352ac9c98b2f8ad240a381148e6
anton-oz <antonosland789@gmail.com>
2025-03-27 15:39:43 -0500

    added markdown preview, updated plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -34,4 +34,13 @@ return {
       underline_links = true,
     },
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
 }
