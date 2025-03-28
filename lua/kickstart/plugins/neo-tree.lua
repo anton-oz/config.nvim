adb7174af9f8efb8fab9bef63063307ad9a08ed1
anton-oz <antonosland789@gmail.com>
2025-03-28 13:09:28 -0500

    added <F2> to show current working directory with neo-tree in full view

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,6 +12,7 @@ return {
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<F2>', ':Neotree current<CR>', desc = 'Neotree full view', silent = true },
   },
   opts = {
     filesystem = {
