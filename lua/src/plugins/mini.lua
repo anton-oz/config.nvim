4729aeda350b87c14c0813578724065be8738fc6
anton <antonosland789@gmail.com>
2026-03-08 01:11:00 -0600

    feat: add catpuccing colors to mini tabline @lua/src/plugins/mini.lua

--- lua/src/plugins/mini.lua
+++ lua/src/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       -- Better Around/Inside textobjects
       --
+      --
       -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
@@ -18,7 +19,43 @@ return {
       require('mini.surround').setup()
 
       require('mini.tabline').setup()
-
+      -- Example: Catppuccin-inspired style
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineCurrent',
+        { fg = '#cdd6f4', bg = '#313244', underline = true, bold = true }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineVisible',
+        { fg = '#a6adc8', bg = '#1e1e2e' }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineHidden',
+        { fg = '#989bb0', bg = '#1e1e2e' }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineModifiedCurrent',
+        { fg = '#f9e2af', bg = '#313244', bold = true }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineModifiedVisible',
+        { fg = '#f9e2af', bg = '#1e1e2e' }
+      )
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineModifiedHidden',
+        { fg = '#f9e2af', bg = '#1e1e2e' }
+      )
+      vim.api.nvim_set_hl(0, 'MiniTablineFill', { bg = '#1e1e2e' })
+      vim.api.nvim_set_hl(
+        0,
+        'MiniTablineTabpagesection',
+        { fg = '#cba6f7', bg = '#1e1e2e', bold = true }
+      )
       -- Simple and easy statusline.
       -- local statusline = require 'mini.statusline'
       -- statusline.setup { use_icons = vim.g.have_nerd_font }
