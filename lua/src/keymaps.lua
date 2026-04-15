59e020f07870775d31cdab930a16d94303b37aca
anton <antonosland789@gmail.com>
2026-04-15 16:48:00 -0500

    feat(keymaps): add toggle colorscheme shortcut, will update colorscheme file for persistance
    - removed unneccesary comments from lua/src/plugins/colorscheme.lua

--- lua/src/keymaps.lua
+++ lua/src/keymaps.lua
@@ -196,3 +196,21 @@ map('n', '<leader>sc', function()
   vim.opt.spell = not vim.opt.spell:get()
   vim.notify('Spell: ' .. (vim.opt.spell:get() and 'ON' or 'OFF'))
 end, { desc = '[s]pell [c]heck' })
+
+map('n', '<leader>tc', function()
+  local current = vim.g.colors_name or ''
+  local new_scheme = current == 'catppuccin-mocha' and 'catppuccin-latte'
+    or 'catppuccin-mocha'
+  vim.cmd('colorscheme ' .. new_scheme)
+
+  local colorscheme_filepath =
+    vim.fn.expand '~/.config/nvim/lua/src/plugins/colorscheme.lua'
+
+  local lines = vim.fn.readfile(colorscheme_filepath)
+  local new_lines = {}
+  for _, line in ipairs(lines) do
+    local new_line = vim.fn.substitute(line, current, new_scheme, '')
+    table.insert(new_lines, new_line)
+  end
+  vim.fn.writefile(new_lines, colorscheme_filepath)
+end, { desc = '[t]oggle [c]olorscheme' })
